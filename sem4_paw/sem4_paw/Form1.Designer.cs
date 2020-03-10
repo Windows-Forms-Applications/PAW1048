8501f49ed866953ed4e9c7ae406e5c6c15696ea8
Cristian Ciurea <cristianciurea@gmail.com>
2020-03-10 20:42:06 +0200

    update

---
+++ sem4_paw/sem4_paw/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace sem4_paw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCod = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbSex = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbVarsta = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbNume = new System.Windows.Forms.TextBox();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cod:";
+            // 
+            // tbCod
+            // 
+            this.tbCod.Location = new System.Drawing.Point(126, 27);
+            this.tbCod.Name = "tbCod";
+            this.tbCod.Size = new System.Drawing.Size(100, 20);
+            this.tbCod.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Adauga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbSex
+            // 
+            this.cbSex.FormattingEnabled = true;
+            this.cbSex.Items.AddRange(new object[] {
+            "M",
+            "F"});
+            this.cbSex.Location = new System.Drawing.Point(126, 65);
+            this.cbSex.Name = "cbSex";
+            this.cbSex.Size = new System.Drawing.Size(121, 21);
+            this.cbSex.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sex:";
+            // 
+            // tbVarsta
+            // 
+            this.tbVarsta.Location = new System.Drawing.Point(126, 113);
+            this.tbVarsta.Name = "tbVarsta";
+            this.tbVarsta.Size = new System.Drawing.Size(100, 20);
+            this.tbVarsta.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Varsta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nume:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Note:";
+            // 
+            // tbNume
+            // 
+            this.tbNume.Location = new System.Drawing.Point(126, 159);
+            this.tbNume.Name = "tbNume";
+            this.tbNume.Size = new System.Drawing.Size(100, 20);
+            this.tbNume.TabIndex = 9;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(126, 205);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(100, 20);
+            this.tbNote.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 293);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.tbNume);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbVarsta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbSex);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbCod);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCod;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbSex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbVarsta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbNume;
+        private System.Windows.Forms.TextBox tbNote;
+    }
+}
---
+++ sem4_paw/sem4_paw/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sem4_paw
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cod = Convert.ToInt32(tbCod.Text);
+                char sex = Convert.ToChar(cbSex.Text);
+                int varsta = Convert.ToInt32(tbVarsta.Text);
+                string nume = tbNume.Text;
+                string[] noteS = tbNote.Text.Split(',');
+                int[] note = new int[noteS.Length];
+                for (int i = 0; i < noteS.Length; i++)
+                    note[i] = Convert.ToInt32(noteS[i]);
+                Student s = new Student(cod, sex, varsta, nume, note);
+                MessageBox.Show(s.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                tbCod.Clear();
+                cbSex.Text = "";
+                tbNume.Clear();
+                tbVarsta.Clear();
+                tbNote.Clear();
+            }
+        }
+    }
+}
